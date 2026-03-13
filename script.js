23656c082445dff7f65bc8e9f70d3f87910bcdb6
Dominatoxz <dominatomatheus06@gmail.com>
2026-03-12 21:23:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const botao1 = document.getElementById('github');
+
+        botao1.addEventListener('click', function () {
+            window.open ('https://github.com/Dominatoxz', '_blank');
+        });
+
+const botao2 = document.getElementById('youtube');
+
+        botao2.addEventListener('click', function () {
+            window.open ('https://www.youtube.com/@MatheusDominato_', '_blank');
+        });
+
+const botao3 = document.getElementById('linkedin');
+
+        botao3.addEventListener('click', function () {
+            window.open ('https://www.linkedin.com/in/matheus-dominato-360737373/', '_blank');
+        });
+
+const botao4 = document.getElementById('snapchat');
+
+        botao4.addEventListener('click', function () {
+            window.open ('https://snapchat.com/t/AatimxeR', '_blank');
+        });
+
+const botao5 = document.getElementById('instagram');
+
+        botao5.addEventListener('click', function () {
+            window.open ('https://www.instagram.com/mthdominato_fx?igsh=amZibjA4ODYwd3Zu&utm_source=qr', '_blank');
+        });
+
+    
